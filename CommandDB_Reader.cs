5b49317a72e143cf89171d1982cc1dc85692cee4
Damien McCormick <DMcCormick20172017@outlook.com>
2023-01-12 10:28:53 -0800

    Add project files.

---
+++ CommandDB_Reader.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DIPsConsoleCompiler
+{
+    public struct Command
+    {
+        public string name;
+        public int id;
+        public string[] args;
+    }
+
+    public class CommandDB_Reader
+    {
+        string[] db;
+        List<Command> commands = new List<Command>();
+        public int errorAt;
+
+        public void init(string dbPath)
+        {
+            db = File.ReadAllLines(dbPath);
+            Console.WriteLine("Parsing CommandDB");
+            errorAt = Parse_DB();
+        }
+
+        int Parse_DB()
+        {
+            for (int i = 0; i < db.Length; i++)
+            {
+                string line = db[i];
+                List<string> args = new List<string>();
+                string arg = "";
+                for (int i2 = 0; i2 < line.Length; i2++)
+                {
+                    if (line[i2] == ',')
+                    {
+                        args.Add(arg);
+                        arg = "";
+                    }
+                    else if (line[i2] == ' ')
+                        continue;
+                    else
+                        arg += line[i2];
+                }
+
+                if (arg != "")
+                    args.Add(arg);
+
+                Command temp = new Command();
+                for (int i2 = 0; i2 < args.Count; i2++)
+                {
+                    int id = new int();
+                    if (Int32.TryParse(args[i2], out id))
+                        temp.id = id;
+                    else if (args[i2][0] == '"' && args[i2][args[i2].Length - 1] == '"')
+                    {
+                        string tempString = args[i2].Remove(0, 1);
+                        tempString = tempString.Remove(tempString.Length - 1, 1);
+                        temp.name = tempString;
+                    }
+                    else
+                    {
+                        if (args[i2].Contains(" ")) return i;
+                        temp.args = parseArgs(args[i2]);
+                    }
+                }
+
+                if (temp.name == null)
+                    temp.name = "null";
+
+                if (args.Count > 0)
+                    commands.Add(temp);
+            }
+            return -1;
+        }
+
+        string[] parseArgs(string arg)
+        {
+            List<string> rtrn = new List<string>();
+            string type = "";
+            while (arg.Length != 0)
+            {
+                if (arg[0] == 'n')
+                {
+                    rtrn.Add("n");
+                    break;
+                }
+                else
+                {
+                    type += arg[0]; type += arg[1];
+                    arg = arg.Remove(0, 2);
+                    rtrn.Add(type);
+                    type = "";
+                    continue;
+                }
+            }
+            return rtrn.ToArray();
+        }
+
+        public bool hasError()
+        {
+            if (errorAt > -1)
+                return true;
+            return false;
+        }
+
+        public int findCommandID(int id)
+        {
+            for (int i = 0; i < commands.Count; i++)
+            {
+                if (id == commands[i].id)
+                    return i;
+            }
+            return -1;
+        }
+
+        public int findCommandName(string name)
+        {
+            for (int i = 0; i < commands.Count; i++)
+            {
+                if (name == commands[i].name)
+                    return i;
+            }
+            return -1;
+        }
+
+        public int getEntryDbId()
+        {
+            for (int i = 0; i < commands.Count; i++)
+            {
+                if (commands[i].id == 0)
+                    return i;
+            }
+            return -1;
+        }
+
+        public Command getCommandInDB(int id)
+        {
+            return commands[id];
+        }
+
+
+        public string[] getArgs(int dbId)
+        {
+            return commands[dbId].args;
+        }
+
+        public void logAllCommands()
+        {
+            for (int i = 0; i < commands.Count; i++)
+            {
+                Console.WriteLine("DB id " + i);
+                Console.WriteLine("Name: " + commands[i].name);
+                Console.WriteLine("ID: " + commands[i].id);
+                Console.WriteLine("Args: ");
+                for (int i2 = 0; i2 < commands[i].args.Length; i2++)
+                    Console.WriteLine("    " + commands[i].args[i2]);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DIPsConsoleCompiler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string inPath = "", outPath = "";
+            bool decompile = false, dbValid = false, bb = false;
+            CommandDB_Reader dB_Reader = new CommandDB_Reader();
+            Program p = new Program();
+            ushort version = 1;
+
+            int argLength = args.Length;
+
+            if (argLength < 2)
+            {
+                Console.WriteLine("Please supply an input file and output path.");
+            }
+            else
+            {
+                inPath = args[0];
+                outPath = args[1];
+                if (argLength >= 3)
+                    dB_Reader.init(args[2]);
+                else
+                    dB_Reader.init(Environment.CurrentDirectory + "/CommandDB/DPScript_1.0.txt");
+
+                for (int i = 4; i < argLength; i++)
+                {
+                    switch (args[i])
+                    {
+                        default:
+                            Console.WriteLine("The argument" + args[i] + "is an invalid argument");
+                            break;
+                        case "-dec":
+                            decompile = true;
+                            break;
+                        case "-rec":
+                            decompile = false;
+                            break;
+                        case "-bb":
+                            bb = true;
+                            break;
+                        case "-ver":
+                            i++;
+                            version = UInt16.Parse(args[i]);
+                            break;
+                    }
+                }
+
+                if (!(dB_Reader.errorAt > -1))
+                    dbValid = true;
+                else
+                    Console.WriteLine("Error Parsing CommandDB at line " + dB_Reader.errorAt);
+            }
+
+            if (dbValid == true)
+            {
+                Console.WriteLine("CommandDB Successfully Parsed!");
+                //dB_Reader.logAllCommands();
+                if (decompile)
+                    p.Decompiler(inPath, outPath, dB_Reader, version);
+                else
+                    p.Compiler(inPath, outPath, dB_Reader, version);
+            }
+
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+
+        public void Decompiler(string inPath, string outPath, CommandDB_Reader db_Reader, ushort version)
+        {
+
+        }
+
+        public void Compiler(string inPath, string outPath, CommandDB_Reader db_Reader, ushort version)
+        {
+            Console.WriteLine("Parsing file for compilation.");
+
+            int entryCount = 0;
+            string[] input = File.ReadAllLines(inPath);
+            bool valid = true;
+            List<int> LineCommand = new List<int>();
+            List<string> LineArg = new List<string>();
+            List<int> fileLines = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string line = input[i];
+                if (line.Contains("(") && line.Contains(")"))
+                {
+                    line = line.Trim();
+                    string com = line.Remove(line.IndexOf("("));
+                    //Console.WriteLine(com);
+                    string arg = line.Remove(0, line.IndexOf("(") + 1);
+                    arg = arg.Remove(arg.IndexOf(")"));
+                    //Console.WriteLine(arg);
+                    if (com.Contains("Command_"))
+                    {
+                        string idString = com.Remove(0, 8);
+                        //Console.WriteLine(idString);
+                        int id = 0;
+                        if (Int32.TryParse(idString, out id))
+                        {
+                            int db_Id = db_Reader.findCommandID(id);
+                            if (db_Id == -1)
+                            {
+                                Console.WriteLine("Invalid command \"" + com + "\"");
+                                return;
+                            }
+                            LineCommand.Add(db_Id);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command \"" + com + "\"");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        int db_Id = db_Reader.findCommandName(com);
+                        if (db_Id == -1)
+                        {
+                            Console.WriteLine("Invalid command \"" + com + "\"");
+                            return;
+                        }
+                        LineCommand.Add(db_Id);
+                    }
+                    LineArg.Add(arg);
+                    fileLines.Add(i);
+                }
+            }
+            Console.WriteLine("Parsing Complete!\nCompiling into binary.");
+
+            /*
+            Console.WriteLine(LineCommand.Count);
+            Console.WriteLine(LineArg.Count);
+            Console.WriteLine(fileLines.Count);
+            */
+
+            int entryId = db_Reader.getEntryDbId();
+            if (entryId == -1)
+            {
+                Console.WriteLine("No entry id in commanddb.");
+                return;
+            }
+
+            foreach (int i in LineCommand)
+            {
+                if (LineCommand[i] == entryId)
+                    entryCount++;
+            }
+
+            List<byte> buffer = new List<byte>();
+            buffer.AddRange(Encoding.ASCII.GetBytes("DPS |").ToList()); buffer.Add(0x00); //header and script type file
+            buffer.AddRange(BitConverter.GetBytes(entryCount)); //entry count
+            buffer.AddRange(BitConverter.GetBytes(version)); //file version
+            string signiture = "DIPsConsoleCompiler"; byte[] signSize = { 0x13, 0x00 };
+            buffer.AddRange(signSize); buffer.AddRange(Encoding.ASCII.GetBytes(signiture).ToList()); //signiture
+            byte[] padding = { 0x00, 0x00, 0x00, 0x00, 0x00 }; buffer.AddRange(padding); //padding at the end of header
+
+            for (int i = 0; i < entryCount; i++)
+            {
+                List<byte> entry = new List<byte>();
+                int entrySize = 0;
+                string[] entryHeader = seperateArgs(LineArg[0]);
+                int sizePlacement = 1;
+                if (compareArgs(db_Reader.getArgs(entryId), entryHeader))
+                {
+                    //Console.WriteLine("Arg compare sucesful");
+                    entryHeader[0] = entryHeader[0].Remove(0, 1); entryHeader[0] = entryHeader[0].Remove(entryHeader[0].Length - 1);
+                    int nameSize = entryHeader[0].Length;
+                    sizePlacement += nameSize;
+                    entry.Add(BitConverter.GetBytes(nameSize)[0]);
+                    entry.AddRange(Encoding.ASCII.GetBytes(entryHeader[0]));
+                    //write size later
+                    if (entryHeader[1] == "true")
+                        entry.Add(0x01);
+                    else
+                        entry.Add(0x00);
+
+                    LineCommand.RemoveAt(0);
+                    LineArg.RemoveAt(0);
+                    fileLines.RemoveAt(0);
+                }
+                else
+                {
+                    Console.WriteLine("Error compiling args at line " + fileLines[0]);
+                    valid = false;
+                    break;
+                }
+
+                while (LineCommand.Count != 0 && LineCommand[0] != entryId)
+                {
+                    Command temp = db_Reader.getCommandInDB(LineCommand[0]);
+                    entry.AddRange(BitConverter.GetBytes(temp.id));
+                    entrySize += 4;
+
+                    if (temp.args[0] != "n")
+                    {
+                        string[] args = seperateArgs(LineArg[0]);
+                        uint argCount = 0;
+                        for (int i2 = 0; i2 < temp.args.Length; i2++)
+                        {
+                            uint typeCount = UInt32.Parse(temp.args[i2][0].ToString());
+                            for (int i3 = (int)argCount; i3 < argCount + typeCount; i3++)
+                            {
+                                switch (temp.args[i2][1])
+                                {
+                                    case 's':
+                                        if (args[i3].StartsWith("\"") && args[i3].EndsWith("\""))
+                                        { args[i3] = args[i3].Remove(0, 1); args[i3] = args[i3].Remove(args[i3].Length - 1, 1); }
+                                        entry.Add(BitConverter.GetBytes(args[i3].Length)[0]);
+                                        entrySize += args[i3].Length + 1;
+                                        entry.AddRange(Encoding.ASCII.GetBytes(args[i3]));
+                                        break;
+                                    case 'u':
+                                        entry.AddRange(BitConverter.GetBytes(UInt32.Parse(args[i3])));
+                                        entrySize += 4;
+                                        break;
+                                    case 'i':
+                                        entry.AddRange(BitConverter.GetBytes(Int32.Parse(args[i3])));
+                                        entrySize += 4;
+                                        break;
+                                    case 'b':
+                                        if (args[i3] == "true")
+                                            entry.Add(0x01);
+                                        else
+                                            entry.Add(0x00);
+                                        entrySize++;
+                                        break;
+                                    case 'h':
+                                        args[i3] = args[i3].Remove(0, 2);
+                                        for (int i4 = 0; i4 < args[i3].Length / 2; i4 += 2)
+                                        {
+                                            string bte = args[i3][i4].ToString(); bte += args[i3][i4 + 1];
+                                            entry.Add(Byte.Parse(bte));
+                                        }
+                                        entrySize += args[i3].Length / 2;
+                                        break;
+                                }
+                            }
+                            argCount += typeCount;
+                        }
+                    }
+                    LineCommand.RemoveAt(0);
+                    LineArg.RemoveAt(0);
+                    fileLines.RemoveAt(0);
+                }
+                //Console.WriteLine(entrySize);
+                entry.InsertRange(sizePlacement, BitConverter.GetBytes(entrySize));
+
+                buffer.AddRange(entry);
+            }
+
+            if (valid)
+            {
+                FileStream output = File.Create(outPath);
+                output.Write(buffer.ToArray(), 0, buffer.Count);
+            }
+
+            Console.WriteLine("Complete!");
+        }
+
+        string[] seperateArgs(string args)
+        {
+            List<string> seperated = new List<string>();
+            string arg = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == ',')
+                {
+                    seperated.Add(arg);
+                    arg = "";
+                }
+                else
+                    arg += args[i];
+            }
+            if (arg != "")
+                seperated.Add(arg);
+
+            for (int i = 0; i < seperated.Count; i++)
+                seperated[i] = seperated[i].Trim();
+
+            return seperated.ToArray();
+        }
+
+        bool compareArgs(string[] args, string[] input)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[0] == "n" && input.Length == 0)
+                    return true;
+                string arg = args[i];
+                int argCount;
+                if (Int32.TryParse(arg[0].ToString(), out argCount) == false)
+                    return false;
+                //Console.WriteLine("Arg count parsed");
+                string type = arg[1].ToString();
+                for (int i2 = i; i2 < argCount + i; i2++)
+                {
+                    switch (type)
+                    {
+                        case "s":
+                            if (!(input[i2].StartsWith("\"") && input[i2].EndsWith("\"")))
+                                return false;
+                            break;
+                        case "i":
+                            if (Int32.Parse(input[i2]) > Int32.MaxValue && Int32.Parse(input[i2]) < Int32.MinValue)
+                                return false;
+                            break;
+                        case "u":
+                            if (UInt32.Parse(input[i2]) > UInt32.MaxValue && UInt32.Parse(input[i2]) < UInt32.MinValue)
+                                return false;
+                            break;
+                        case "b":
+                            if (input[i2] != "true" && input[i2] != "false")
+                                return false;
+                            break;
+                        case "h":
+                            if (!(input[i2].StartsWith("0x")))
+                                return false;
+                            break;
+                    }
+                    //Console.WriteLine("Arg " + i + " parsed");
+                }
+            }
+            return true;
+        }
+    }
+}
